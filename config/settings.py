b8c67ac84d657b739ce33dd8089bd4ca27b4980b
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2026-07-31 06:59:33 +0530

    static files added to static root with collectstatic

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
