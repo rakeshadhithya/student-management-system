aea54416dddc34696a5a3d8bf936a05282bdaeb9
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2026-07-31 07:29:13 +0530

    urlpatterns changed to server media files on server

--- config/urls.py
+++ config/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('mentors/', include('mentors.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
